71df5bcd22d0126ccb8ee753c3aa7f14f5151b7a
slashxu <sergiojsjr@yahoo.com>
2023-04-06 14:49:28 -0300

    Animacoes home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,6 +5,8 @@ import { useNavigation } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons'
 import { Logo } from '../../components/logo'
 
+import { Text as MotiText} from 'moti'
+
 import { FoodList } from '../../components/foodlist'
 
 import api from '../../services/api'
@@ -37,8 +39,38 @@ export function Home() {
       
       <Logo/>
 
-      <Text style={styles.title}>Encontre a receita</Text>
-      <Text style={styles.title}>que combina com você</Text>
+      <MotiText 
+        style={styles.title}
+        from={{
+          opacity:0,
+          translateY: 15,
+        }}
+        animate={{
+          opacity: 1,
+          translateY: 0,
+        }}
+        transition={{
+          delay: 100,
+          type: 'timing',
+          duration: 650,          
+        }}
+        >Encontre a receita</MotiText>
+      <MotiText 
+        style={styles.title}
+        from={{
+          opacity:0,
+          translateY: 18,
+        }}
+        animate={{
+          opacity: 1,
+          translateY: 0,
+        }}
+        transition={{
+          delay: 200,
+          type: 'timing',
+          duration: 850,          
+        }}
+        >que combina com você</MotiText>
 
       <View style={styles.form}>
         <TextInput
