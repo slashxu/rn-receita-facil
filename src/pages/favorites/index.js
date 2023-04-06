b0cac952bf274d7bd4c2e92df4ca29b0b49b0575
slashxu <sergiojsjr@yahoo.com>
2023-04-06 14:00:28 -0300

    Implementado page Favoritos

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,15 +1,64 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { useIsFocused } from '@react-navigation/native'
+
+import { getFavorites } from '../../utils/storage'
+import { FoodList } from '../../components/foodlist'
 
 export function Favorites() {
+  const [receipe, setreceipe] = useState([])
+  const isFocused = useIsFocused()
+
+  useEffect(() => {
+    let isActive = true
+
+    async function getReceipes(){
+
+      const result = await getFavorites('@appreceitas')
+      if(isActive){
+        setreceipe(result)
+      }
+    }
+
+    if(isActive){
+      getReceipes();
+    }
+
+    return () => {
+      isActive = false
+    }
+  }, [isFocused])
+
  return (
-   <View style={styles.container}>
-    <Text>Pagina Favoritos !!!</Text>
-   </View>
+   <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Receitas Favoritas</Text>
+
+    { receipe.length === 0 && (
+      <Text>Você ainda não tem nenhuma receita salva.</Text>
+    )}  
+
+    <FlatList
+      style={{ marginTop: 14 }}
+      data={receipe}
+      keyExtractor={(item) => String(item.id)}
+      renderItem={({ item }) => <FoodList data={item} /> }
+      showsVerticalScrollIndicator={false}
+    />
+   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'red'
+      flex: 1,
+      backgroundColor: '#f3f9ff',
+      paddingStart: 14,
+      paddingEnd:14,
+      paddingTop: 36,
+    },
+    title:{
+      color: '#000',
+      fontWeight: 'bold',
+      fontSize: 24,
     }
 })
---
+++ src/utils/storage.js
@@ -0,0 +1,44 @@
+import  AsyncStorage from '@react-native-async-storage/async-storage'
+
+export async function getFavorites(key){
+    const favorites = await AsyncStorage.getItem(key)
+    return JSON.parse(favorites) || [];
+}
+
+export async function saveFavorite(key, newItem){
+    let myFavorites = await getFavorites(key)
+    let hasItem = myFavorites.some( item => item.id === newItem.id)
+
+    if(hasItem){
+        console.log('Esse item ja existe salvo na sua lista')
+        return;
+    }
+
+    myFavorites.push(newItem)
+
+    await AsyncStorage.setItem(key, JSON.stringify(myFavorites))
+    console.log('Item salvo com sucesso')
+}
+
+export async function removeFavorite(id){
+    let receipes = await getFavorites('@appreceitas')
+
+    let myFavorites = receipes.filter( item => {
+        return (item.id !== id)
+    })
+
+    await AsyncStorage.setItem('@appreceitas', JSON.stringify(myFavorites))
+    console.log('Item deletado com sucesso')
+    return myFavorites;
+}
+
+export async function isFavorite(receipe){
+    let myReceipes = await getFavorites('@appreceitas')
+
+    const favorite = myReceipes.find( item => item.id === receipe.id)
+
+    if(favorite){
+        return true;
+    }
+    return false;
+}
